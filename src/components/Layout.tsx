224249e12fca99ca44c36a15a440294436544673
Seivi <brunning.fabio@gmail.com>
2025-10-02 07:55:47 -0300

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -181,7 +181,7 @@ export default function Layout({ children }: LayoutProps) {
           <img
             src="/logo-confraria.png"
             alt="Confraria Pedra Branca"
-            className="h-10 w-auto object-contain"
+            className="h-12 w-auto object-contain"
           />
         </div>
         <div className="w-10"></div>
