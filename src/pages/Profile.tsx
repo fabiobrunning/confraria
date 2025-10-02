1dfae0ea67823fed912a43cbd00a88ea92ae38b7
Seivi <brunning.fabio@gmail.com>
2025-10-02 07:12:51 -0300

    Updated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,7 +8,7 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/hooks/use-toast";
-import { Loader2, Plus, Trash2, Search } from "lucide-react";
+import { Loader as Loader2, Plus, Trash2, Search } from "lucide-react";
 import { fetchAddressByCep, fetchCompanyByCnpj } from "@/utils/apis";
 import {
   AlertDialog,
